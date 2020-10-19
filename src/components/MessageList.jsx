86b860a5b868c54b055578b1ecfb15cd109c29f6
Shaiah Wren <sde2111@columbia.edu>
2020-10-19 15:24:53 -0400

    finished testing

---
+++ src/components/MessageList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MessageList = props => {
+    const { messages } = props;
+    return (
+        <ul>
+            {messages.map((message, index) => (
+                <li key={`message-${index}`>{message}}>{message}</li>
+            ))}
+
+        </ul>
+    )
+}
+
+
+export default MessageList;
